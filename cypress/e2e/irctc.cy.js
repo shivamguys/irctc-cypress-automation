bd3405763206348ba91cdf31b8052bb5750ccf99
Shivam Pandey <shivamguys@gmail.com>
2024-04-11 15:13:26 +0530

    timeout -> 90000ms

--- cypress/e2e/irctc.cy.js
+++ cypress/e2e/irctc.cy.js
@@ -12,7 +12,8 @@ describe('IRCTC TATKAL BOOKING', () => {
 
     cy.viewport(1478, 1056)
     cy.visit('https://www.irctc.co.in/nget/train-search', {
-      failOnStatusCode: false
+      failOnStatusCode: false,
+      timeout: 90000
     })
 
 
